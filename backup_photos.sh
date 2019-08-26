870a04854bcf26b8cef3eed7a90811bf3b1b5c33
dawidkotarba <dawidkotarba>
2019-08-26 13:30:17 +0200

    Update script

--- backup_photos.sh
+++ backup_photos.sh
@@ -2,6 +2,7 @@
 HOST="192.168.1.15 9999"
 USER="anonymous"
 PASSWORD="anonymous"
+BACKUP_FOLDER1=`echo L21lZGlhL3pha29jaGFuaS9QbGlraS9GT1RZL1BvY29waG9uZV9SZWRtaQo= | base64 --decode`
 
 ftp -inv $HOST <<EOF
 user $USER $PASSWORD
@@ -10,3 +11,5 @@ binary
 mget *.jpg
 bye
 EOF
+
+cp *.jpg $BACKUP_FOLDER1
